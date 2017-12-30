45b5134b39df751842bec72a9d4c22d2e9889c7d
FreeHandz <hejjanorbo@gmail.com>
2017-12-30 13:58:21 +0100

    bomb

---
+++ newYearGameJam/Assets/Scripts/Bomb.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour {
+
+    // mennyi idő múlva robban
+    public float detonationTime = 1f;
+
+    // bomba counter, növeljük frissítésnél, 
+    // ha eléri a detonationTimet, akkor felrobban
+    public float currentBombType = 0f;
+    
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // növeljük a timert a robbanásig
+        currentBombType += 0.01f;
+
+        if (currentBombType >= detonationTime)
+        {
+            // TODO create new explosion
+            // TODO pooling
+            Destroy(GetComponent<SpriteRenderer>());
+        }
+    }
+}
